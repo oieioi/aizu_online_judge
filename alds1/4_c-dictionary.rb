88e95461d6411efbd7ea654dbcee11840ac3670d
oieioi <atsuinatsu.samuifuyu@gmail.com>
2016-04-26 18:47:10 +0900

    wip

--- alds1/4_c-dictionary.rb
+++ alds1/4_c-dictionary.rb
@@ -1,55 +1,110 @@
 class Dictionary
-  DNA = %w(A C G T)
+  DNA = [''] + %w(A C G T)
 
-  def initialize
-    @container = Array.new(64)
+  def initialize n
+    @size = 100000
+    @container = Array.new(@size)
+    @ret = []
+  end
+  attr_accessor :ret
+
+  def exec command, str = ''
+    case command
+    when 'insert' then insert str
+    when 'find'   then @ret << (find?(str) ? 'yes' : 'no')
+    else raise 'へん'
+    end
   end
 
-  def exec command, str
-   case command
-   when 'insert' then insert str
-   when 'find'   then puts(find?(str) ? 'yes' : 'no')
-   else raise 'へん'
-   end
-  end
-
-  private
+  #private
 
   def insert str
-    @container[hash(str)] = true
+    #puts "#{str} : #{hash(str)}"
+    if @container[hash(str)].nil?
+      @container[hash(str)] = LinkList.new str
+    else
+      group = @container[hash(str)]
+      found = group.has? str
+      group.insert(str) unless found
+    end
   end
 
   def find? str
-    not find(str).nil?
-  end
+    linked_list = @container[hash(str)]
+    return false if linked_list.nil?
 
-  def find str
-    @container[hash(str)] 
+    linked_list.has?(str)
   end
 
   def hash str
-    base = 4
-    # AAA 〜 TTT までの数字を4進数 -> 10進数に変換する
-    str.chars.map {|c|
-      case c
-      when DNA[0] then '0'
-      when DNA[1] then '1'
-      when DNA[2] then '2'
-      when DNA[3] then '3'
-      else raise 'へん'
+    # 超素朴な実装
+    # AAA 〜 TTT -> 4進数 -> 10進数に変換する
+    base = DNA.size
+    hash_value = str.
+      # 文字列を4進数で表現する
+      chars.map {|c|
+      index = get_dna_index c
+      raise "おかしな値 #{c}" if index.nil?
+      index
+    }.
+    # 各桁をそれぞれ10進数にする
+    reverse.map.with_index {|n, index|
+      # (底 ** 何桁目の値か) * 値
+      # 333 -> 63 ((4**2) * 3) + ((4**1) * 3) + ((4**0) * 3)
+      (base ** index) * n.to_i
+    }.
+    # 各桁毎に出した10進数の値を全て足す
+    reduce {|memo, item| memo + item }
+
+    #puts @size
+    hash_value % @size
+  end
+
+  def get_dna_index char
+    # 妥協
+    DNA.index char
+  end
+
+  class LinkList
+    def initialize first_value
+      @first = Node.new first_value, nil
+    end
+    def insert key
+      new_node = Node.new key, @first
+      @first = new_node
+    end
+    def has? key
+      current = @first
+      i = 0
+      until current.nil?
+        i += 1
+        if current.key == key
+          return true
+        else
+          current = current.next
+        end
       end
-    }
-      .reverse.map.with_index { |n, index| (base * index) * n.to_i }
-      .reduce {|memo, item| memo + item}
+      false
+    end
+    # リンクリスト用のアイテム
+    class Node
+      def initialize key, next_node
+        @key = key
+        @next = next_node
+      end
+      attr_accessor :key, :next
+    end
   end
 end
 
 def main size, commands
-  dic = Dictionary.new
+  dic = Dictionary.new size.to_i
   commands.each{|raw_command|
     command, str = raw_command.split ' '
     dic.exec(command, str)
   }
+  puts dic.ret.join "\n"
+  #dic.container.each {|pii| p pii}
 end
 
 array = STDIN.read.split(/\n/)
