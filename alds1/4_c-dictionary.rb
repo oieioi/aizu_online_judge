74946adfbf89827fe2c4a33109ca7f52fc6178bb
oieioi <atsuinatsu.samuifuyu@gmail.com>
2016-04-27 10:47:54 +0900

    Pass

--- alds1/4_c-dictionary.rb
+++ alds1/4_c-dictionary.rb
@@ -2,24 +2,22 @@ class Dictionary
   DNA = [''] + %w(A C G T)
 
   def initialize n
-    @size = 100000
+    @size = n
     @container = Array.new(@size)
-    @ret = []
   end
   attr_accessor :ret
 
   def exec command, str = ''
     case command
     when 'insert' then insert str
-    when 'find'   then @ret << (find?(str) ? 'yes' : 'no')
+    when 'find'   then puts(find?(str) ? 'yes' : 'no')
     else raise 'へん'
     end
   end
 
-  #private
+  private
 
   def insert str
-    #puts "#{str} : #{hash(str)}"
     if @container[hash(str)].nil?
       @container[hash(str)] = LinkList.new str
     else
@@ -37,47 +35,42 @@ class Dictionary
   end
 
   def hash str
-    # 超素朴な実装
-    # AAA 〜 TTT -> 4進数 -> 10進数に変換する
-    base = DNA.size
-    hash_value = str.
-      # 文字列を4進数で表現する
-      chars.map {|c|
-      index = get_dna_index c
-      raise "おかしな値 #{c}" if index.nil?
-      index
-    }.
-    # 各桁をそれぞれ10進数にする
-    reverse.map.with_index {|n, index|
-      # (底 ** 何桁目の値か) * 値
-      # 333 -> 63 ((4**2) * 3) + ((4**1) * 3) + ((4**0) * 3)
-      (base ** index) * n.to_i
-    }.
-    # 各桁毎に出した10進数の値を全て足す
-    reduce {|memo, item| memo + item }
-
-    #puts @size
-    hash_value % @size
+    return str.hash % @size
+    # 超素朴な実装 (おそすぎ)
+    # AGCT -> 4進数 -> 10進数に変換する
+    #base = DNA.size
+    #size = str.size
+    #sum = 0
+    #str.chars.each.with_index {|c, index|
+    #  # 文字列を4進数で表現する
+    #  n = get_dna_index c
+    #  # 各桁をそれぞれ10進数にする
+    #  # (底 ** 何桁目の値か) * 値
+    #  # 各桁毎に出した10進数の値を全て足す
+    #  sum += (base ** (size - index)) * n
+    #}
+    #sum % @size
   end
 
   def get_dna_index char
-    # 妥協
     DNA.index char
   end
 
+  # 各ハッシュテーブルに格納するリンクリスト
   class LinkList
+
     def initialize first_value
       @first = Node.new first_value, nil
     end
+
     def insert key
       new_node = Node.new key, @first
       @first = new_node
     end
+
     def has? key
       current = @first
-      i = 0
       until current.nil?
-        i += 1
         if current.key == key
           return true
         else
@@ -86,6 +79,7 @@ class Dictionary
       end
       false
     end
+
     # リンクリスト用のアイテム
     class Node
       def initialize key, next_node
@@ -103,8 +97,6 @@ def main size, commands
     command, str = raw_command.split ' '
     dic.exec(command, str)
   }
-  puts dic.ret.join "\n"
-  #dic.container.each {|pii| p pii}
 end
 
 array = STDIN.read.split(/\n/)
