ddcc103f459be640085dc96e57f8c5379d7e972b
oieioi <atsuinatsu.samuifuyu@gmail.com>
2016-07-14 23:32:13 +0900

    memorize

--- alds1/5_a-divide_and_conquer.rb
+++ alds1/5_a-divide_and_conquer.rb
@@ -1,26 +1,39 @@
 # sumをつくれるか
-def can_assemble?(elements, sum)
-  elements.size.times.each_with_index { |size|
-    elements.combination(size){ |nums|
-      return true if (nums.reduce{|n,s| n + s }) == sum
+module Main
+  extend self
+
+  @@memo = {}
+  def can_assemble?(elements, index, sum, raw_elements_length)
+    unless @@memo[index].nil?
+      return @@memo[index][sum] unless @@memo[index][sum].nil?
+    else
+      @@memo[index] = []
+    end
+
+    if sum == 0
+      true
+    elsif index >= raw_elements_length
+      false
+    else
+      p index
+      @@memo[index][sum] = can_assemble?(elements, index + 1, sum, raw_elements_length) || can_assemble?(elements, index + 1, sum - elements[index], raw_elements_length)
+      @@memo[index][sum]
+    end
+  end
+
+  def main
+    lines = STDIN.read.split(/\n/)
+    lines.shift
+
+    elements = lines.shift.split(' ').map{|s|s.to_i}
+    lines.shift
+
+    sums = lines.shift.split(' ').map{|s|s.to_i}
+    sums.each{ |sum|
+      puts can_assemble?(elements, 0, sum, elements.size) ? 'yes' : 'no'
     }
-  }
 
-  false
+  end
 end
 
-def main
-  lines = STDIN.read.split(/\n/)
-  lines.shift
-
-  elements = lines.shift.split(' ').map{|s|s.to_i}
-  lines.shift
-
-  sums = lines.shift.split(' ').map{|s|s.to_i}
-  sums.each{ |sum|
-    puts can_assemble?(elements, sum) ? 'yes' : 'no'
-  }
-
-end
-
-main
+Main::main
